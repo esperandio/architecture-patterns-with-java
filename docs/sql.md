f80f783b5d87dddbe80c40dcaaca944c0f0abadb
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2023-05-14 14:01:00 +0000

    test: criacao de teste para validar a recuperacao de dados do banco usando o hibernate

--- docs/sql.md
+++ docs/sql.md
@@ -1,11 +1,8 @@
 ```sql
-CREATE TABLE test.song (
-	songId INT auto_increment NOT NULL,
-	singer varchar(100) NULL,
-	songName varchar(100) NULL,
-	CONSTRAINT song_PK PRIMARY KEY (songId)
+CREATE TABLE Batches (
+    Reference varchar(100) NULL,
+    Sku varchar(100) NULL,
+    PurchasedQuantity INT NULL
 )
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_0900_ai_ci;
+ENGINE=InnoDB;
 ```
---
+++ persistence/src/test/java/app/persistence/SessionTest.java
@@ -0,0 +1,63 @@
+package app.persistence;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import app.domain.*;
+
+public class SessionTest {
+    private Session session;
+
+    public SessionTest() {
+        var configuration = new Configuration();
+
+        configuration.configure("hibernate.cfg.xml");        
+        configuration.addAnnotatedClass(Batch.class);
+        configuration.addResource("Batch.hbm.xml");
+
+        // Create Session Factory
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+ 
+        // Initialize Session Object
+        this.session = sessionFactory.openSession();
+    }
+
+    @BeforeEach
+    public void initEach() {
+        this.session.doWork(connection -> {
+            connection.prepareStatement("TRUNCATE TABLE Batches").executeUpdate();
+        });
+    }
+
+    @Test
+    void testCanRetrieveBatches()
+    {
+        this.session.beginTransaction();
+
+        this.session.doWork(connection -> {
+            connection.prepareStatement("INSERT INTO Batches (Reference, Sku, PurchasedQuantity) VALUES ('batch-001', 'SMALL-TABLE', 20)").executeUpdate();
+            connection.prepareStatement("INSERT INTO Batches (Reference, Sku, PurchasedQuantity) VALUES ('batch-002', 'SMALL-TABLE', 20)").executeUpdate();
+            connection.prepareStatement("INSERT INTO Batches (Reference, Sku, PurchasedQuantity) VALUES ('batch-003', 'SMALL-TABLE', 20)").executeUpdate();
+        });
+
+        this.session.getTransaction().commit();
+
+        List<Batch> actual = this.session.createQuery("FROM Batch", Batch.class).list();
+        List<Batch> expected = Arrays.asList(
+            new Batch("batch-001", "SMALL-TABLE", 20),
+            new Batch("batch-002", "SMALL-TABLE", 20),
+            new Batch("batch-003", "SMALL-TABLE", 20)
+        );
+
+        assertEquals(3, actual.size());
+        assertIterableEquals(expected, actual);
+    }
+}
